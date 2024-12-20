ececbb0d3f447cb415e5818bed7237e31f5d2fe4
JJMR2310 <matajefry2@gmail.com>
2024-12-20 00:08:13 -0600

    Estilo y contenido de landingpage

--- src/components/Landingpage/MainContent.js
+++ src/components/Landingpage/MainContent.js
@@ -3,9 +3,18 @@ import React from "react";
 const MainContent = () => {
     return(
         <main>
-            <section id="servicies">
-                <h2>Nuestros Servicio</h2>
-                <p>Descripcion de los servicios ofrecidos</p>
+            <section className="HeroSection">
+                <h2>Organiza tus tareas facilmente</h2>
+                <p>Crea, organiza y realiza un seguimiento de tus proyectos con facilidad.</p>
+                <button className="Register">Regístrate Gratis</button>
+            </section>
+            <section id="FeaturesSection">
+                <h3>Características Clave</h3>
+                <p>Organiza tus tareas de forma visual e intuitiva</p>
+                <h4>Temporizadores</h4>
+                <p>Optimiza tu tiempo con técnicas de productividad</p>
+                <h4>Integraciones</h4>
+                <p>Añade funcionalidad con APIs como OpenWeather y YouTube</p>
             </section>
         </main>
     );
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,13 +2,13 @@ import React from "react";
 import Header from "../components/Landingpage/Header";
 import MainContent from "../components/Landingpage/MainContent";
 import Footer from "../components/Landingpage/Footer";
-
+import "../styles/StylesLanding/LandingPage.css";
 const Landingpage = () =>{
     return (
-        <div>
-            <Header/>
-            <MainContent/>
-            <Footer/>
+        <div className="content">
+            <div className="nav"><Header/></div>
+            <div className="main"><MainContent/></div>    
+            <div className="footer"><Footer/></div>
         </div>
     );
 };
